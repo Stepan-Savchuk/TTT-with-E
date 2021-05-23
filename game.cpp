c41144eb551ff3d3fb9fa3a5a2c1e83cb1f5013d
Stepan Savchuk <igrok.nikson.230@gmail.com>
2021-05-23 19:37:41 +0300

    Unfinished work on replacement conio.h input on windows.h input

--- game.cpp
+++ game.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 #include <string>
 #include <stdio.h>
-#include <conio.h>
 #include <windows.h>
 
 using namespace std;
@@ -9,6 +8,22 @@ using namespace std;
 const int sizeOfMap = 4;
 const int baseX = 0;
 const int baseY = 0;
+const int actionKeys[] = {100, 119, 97, 115};
+
+void ErrorExit(LPCSTR lpszMessage){
+  cout << lpszMessage;
+  ExitProcess(0);
+}
+
+bool isActionKey(int key){
+  int quantityOfActionKeys = 4;
+  for(int i=0; i < quantityOfActionKeys; i++){
+    if(key == actionKeys[i]){
+      return true;
+    }
+  }
+  return false;
+}
 
 void setMap(int size, COORD position, char mp[sizeOfMap][sizeOfMap]){
   for(int i=0; i < size; i++){
@@ -39,20 +54,37 @@ void drawMap(char mp[sizeOfMap][sizeOfMap], int size, HANDLE stream){
   SetConsoleCursorPosition(stream, c);
 }
 
-void input(int key, COORD& position){
-  if(key==100){
+void movement(int key, COORD& position){
+  if(key==actionKeys[0]){
     position.X++;
-  } else if(key==119){
+    cout << actionKeys[0];
+  } else if(key==actionKeys[1]){
     position.Y++;
-  } else if(key==97){
+    cout << actionKeys[1];
+  } else if(key==actionKeys[2]){
     position.X--;
-  } else if(key==115){
+    cout << actionKeys[2];
+  } else if(key==actionKeys[3]){
     position.Y--;
+    cout << actionKeys[3];
   }
 }
 
 int main(){
+  HANDLE hStdin;
   HANDLE hStdout = GetStdHandle(STD_OUTPUT_HANDLE);
+
+  const short bufferSize = 128;
+  INPUT_RECORD irInBuf[bufferSize];
+
+  hStdin = GetStdHandle(STD_INPUT_HANDLE);
+  if(hStdin == INVALID_HANDLE_VALUE){
+    ErrorExit("GetStdHandle");
+  }
+
+  int index = 0;
+  int eventsCount = 0;
+  
   COORD size;
   size.X = 80;
   size.Y = 40;
@@ -75,16 +107,26 @@ int main(){
   drawMap(mp, sizeOfMap, hStdout);
   
   while(true){
-    bool somethingPressed = _kbhit();
+    DWORD cNumRead = 0;
+    BOOL peekSuccessful = PeekConsoleInput(hStdin, irInBuf, bufferSize, &cNumRead);
+
+    if(!peekSuccessful) ErrorExit("PeekConsoleInput");
+    if(!FlushConsoleInputBuffer(hStdin)) ErrorExit("FlushConsoleInputBuffer");
+
+    eventsCount += cNumRead;
+    cout << " iteration " << index << " total " << eventsCount << " current " << cNumRead; 
+    
+    bool somethingPressed = true;
     if(somethingPressed){
-      system("CLS");
-      int key = _getch();
-      input(key, position);
+      int key = 0;
+      movement(key, position);
       if(key == 13){
 	break;
+      } else if(isActionKey(key)){
+	system("CLS");
+	setMap(sizeOfMap, position, mp);
+	drawMap(mp, sizeOfMap, hStdout);
       }
-      setMap(sizeOfMap, position, mp);
-      drawMap(mp, sizeOfMap, hStdout);
     }
   }
   

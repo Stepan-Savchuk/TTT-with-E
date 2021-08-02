413a5bab2ead641f8836398b7d081c0ea234f827
Stepan Savchuk <igrok.nikson.230@gmail.com>
2021-08-02 21:57:49 +0300

    Learning SDL Alpha Blending

--- TTT.cpp
+++ TTT.cpp
@@ -17,6 +17,9 @@ public:
 
   void setColor(Uint8 red, Uint8 green, Uint8 blue);
 
+  void setBlendMode(SDL_BlendMode blending);
+  void setAlpha(Uint8 alpha);
+
   void render(int x, int y, SDL_Rect* clip = NULL);
 
   int getWidth();

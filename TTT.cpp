5ca889169fd92c0a09ff02e5f5906c5bd2711fae
Nikson230 <igrok.nikson.230@gmail.com>
2021-09-24 10:19:54 +0300

    I have changed font in TTT(again) and made multiline rendering with reading of keyStates

--- TTT.cpp
+++ TTT.cpp
@@ -5,8 +5,11 @@
 #include<cmath>
 
 
+
 const int SCREEN_WIDTH = 640;
 const int SCREEN_HEIGHT = 480;
+const int MAP_SIZE = 3;
+
 
 
 class WTexture {
@@ -32,15 +35,18 @@ public:
   }
 };
 
+
 WTexture::WTexture(){
   texture = NULL;
   width = 0;
   height = 0;
 }
 
+
 WTexture::~WTexture(){
   free();
 }
+
 
 void WTexture::free(){
   SDL_DestroyTexture(texture);
@@ -49,6 +55,7 @@ void WTexture::free(){
   width = 0;
   height = 0;
 }
+
 
 bool WTexture::loadFromString(TTF_Font* font, std::string str, SDL_Color textColor, SDL_Renderer* renderer){
     free();
@@ -74,6 +81,7 @@ bool WTexture::loadFromString(TTF_Font* font, std::string str, SDL_Color textCol
     return texture!=NULL;
 }
 
+
 void WTexture::render(int x, int y, SDL_Renderer* renderer){
   SDL_QueryTexture(texture, NULL, NULL, &height, &width);
   SDL_Rect renderRect = {x, y, height, width};
@@ -82,9 +90,98 @@ void WTexture::render(int x, int y, SDL_Renderer* renderer){
 }
 
 
-bool init(SDL_Window* window, SDL_Renderer* renderer);
-bool loadFont(TTF_Font *font, WTexture texture, std::string str, SDL_Renderer* renderer);
-void close(SDL_Window* window, SDL_Renderer* renderer);
+
+bool init(SDL_Window** window, SDL_Renderer** renderer);
+bool loadFont(TTF_Font *font, WTexture* texture, std::string str, SDL_Renderer* renderer);
+void createMap(std::string strArr[MAP_SIZE], bool toFill, int size);
+void close(SDL_Window* window, SDL_Renderer* renderer, TTF_Font* font);
+
+
+
+void createMap(std::string strArr[MAP_SIZE], int size, bool toFill){
+  for(int i=0; i < size; i++){
+    if(strArr[i] != ""){
+      strArr[i] = "";
+    }
+    for(int j=0; j < size; j++){
+      if(toFill){
+	strArr[i] += 'O';
+      } else {
+	strArr[i] += ' ';
+      }
+    }
+  }
+}
+
+
+int main(int argc, char *argv[]){
+  SDL_Window* window = NULL;
+  SDL_Renderer* globalRenderer;
+
+  TTF_Font* font = NULL;
+
+  std::string map = "Hello";
+
+  std::string testMap[] = {"",
+    "",
+  ""};
+
+  createMap(testMap, MAP_SIZE, true);
+
+  for(int i=0; i < MAP_SIZE; i++){
+    printf("%s\n", testMap[i].c_str());
+  }
+
+  WTexture mapTexture;
+  
+  if(!init(&window, &globalRenderer)){
+    printf("Error with init\n");
+  } else {
+      bool quit = false;
+      bool toShow = true;
+      
+      SDL_Event event;
+      while(!quit){
+	while(SDL_PollEvent(&event) != 0){
+	  if(event.type == SDL_QUIT){
+	    quit = true;
+	  }
+	}
+	const Uint8* keyState = SDL_GetKeyboardState(NULL);
+	if(keyState[SDL_SCANCODE_UP]){
+	  printf("UP\n");
+	} else if(keyState[SDL_SCANCODE_DOWN]){
+	  printf("DOWN\n");
+	} else if(keyState[SDL_SCANCODE_LEFT]){
+	  printf("LEFT\n");
+	} else if(keyState[SDL_SCANCODE_RIGHT]){
+	  printf("RIGHT\n");
+	} else {
+	  printf("0\n");
+	}
+
+	if(toShow){
+	  SDL_RenderClear(globalRenderer);
+	  for(int i=0; i < MAP_SIZE; i++){
+	    if(!loadFont(font, &mapTexture, testMap[i], globalRenderer)){
+	      printf("Error with font loading\n");
+	    } else {
+	      SDL_SetRenderDrawColor(globalRenderer, 0x00, 0x00, 0x00, 0xFF);
+
+	      mapTexture.render(0, i*25, globalRenderer);//const value to make multiline drawing
+
+	      SDL_RenderPresent(globalRenderer);
+	    }
+	  }
+	  toShow = false;
+	}
+      }
+  }
+  close(window, globalRenderer, font);
+  
+  return 0;
+}
+
 
 
 bool init(SDL_Window** window, SDL_Renderer** renderer){
@@ -106,11 +203,11 @@ bool init(SDL_Window** window, SDL_Renderer** renderer){
   return true;
 }
 
-bool loadFont(TTF_Font *font, WTexture* texture, std::string str, SDL_Renderer* renderer){
-  font = TTF_OpenFont("Raleway-Black.ttf", 64);
 
+bool loadFont(TTF_Font* font, WTexture* texture, std::string str, SDL_Renderer* renderer){
+  font = TTF_OpenFont("Courier Prime.ttf", 32);
   if(font == NULL){
-    printf("Falied to load font! SDL_ttf Error %s\n", TTF_GetError());
+    printf("Falied to load font! SDL_ttf Error: %s\n", TTF_GetError());
     return false;
   } else {
     SDL_Color textColor = {0xFF,0xFF,0xFF};
@@ -123,51 +220,18 @@ bool loadFont(TTF_Font *font, WTexture* texture, std::string str, SDL_Renderer* 
   return true;
 }
 
-void close(SDL_Window* window, SDL_Renderer* renderer){
+
+
+
+
+void close(SDL_Window* window, SDL_Renderer* renderer, TTF_Font* font){
   SDL_DestroyRenderer(renderer);
   renderer = NULL;
 
   SDL_DestroyWindow(window);
   window = NULL;
 
+  font = NULL;
+
   SDL_Quit();
-}
-
-int main(int argc, char *argv[]){
-  SDL_Window* window = NULL;
-  SDL_Renderer* globalRenderer;
-
-  TTF_Font *font = NULL;
-
-  std::string map = "Hello Count 0";
-
-  WTexture mapTexture;
-  
-  if(!init(&window, &globalRenderer)){
-    printf("Error with init\n");
-  } else {
-    if(!loadFont(font, &mapTexture, map, globalRenderer)){
-      printf("Error with font loading\n");
-    } else {
-      bool quit = false;
-      SDL_Event event;
-      while(!quit){
-	while(SDL_PollEvent(&event) != 0){
-	  if(event.type == SDL_QUIT){
-	    quit = true;
-	  }
-	}
-	SDL_SetRenderDrawColor(globalRenderer, 0x00, 0x00, 0x00, 0xFF);
-	
-	SDL_RenderClear(globalRenderer);
-
-	mapTexture.render(0, 0, globalRenderer);
-
-	SDL_RenderPresent(globalRenderer);
-      }
-    }
-  }
-  close(window, globalRenderer);
-  
-  return 0;
 }

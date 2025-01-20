57d769bce69d36cb682ca6b540e48b08d9cffcb3
Stepan-Savchuk <stepan_savchuk3@protonmail.com>
2025-01-20 11:37:29 +0200

    Added to X and O sprites to game

--- main.lua
+++ main.lua
@@ -1,8 +1,8 @@
 function love.load()
-  x_sprite = love.graphics.newImage("X.png")
-  o_sprite = love.graphics.newImage("O.png")
+  x_sprite = love.graphics.newImage("X2.png")
+  o_sprite = love.graphics.newImage("O2.png")
 end
 function love.draw()
   love.graphics.draw(x_sprite, 16, 16)
-  love.graphics.draw(o_sprite, 32, 32)
+  love.graphics.draw(o_sprite, 256, 256)
 end

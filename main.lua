d62bff814288c79e334cff19c62d890830d5e5de
Stepan-Savchuk <stepan_savchuk3@protonmail.com>
2025-02-10 09:49:56 +0200

    Polished aftermath

--- main.lua
+++ main.lua
@@ -1,7 +1,7 @@
---polish check for win(disable any action after win)
+--add a menu
 
 function love.load()
-X_sprite = love.graphics.newImage("X2.png")
+  X_sprite = love.graphics.newImage("X2.png")
   O_sprite = love.graphics.newImage("O2.png")
 
   Grid = {}
@@ -18,32 +18,42 @@ X_sprite = love.graphics.newImage("X2.png")
   O_Eraser = 1
 
   Win = false
+  InGame = true
 end
 
+function love.update()
+  if Win then
+    InGame = false
+  end
+end
 
 function love.draw()
-  local testString = "Player " ..Turn
-  love.graphics.print(testString, (129*3)+64, 32, 0, 4, 4)
-  love.graphics.print("X Eraser left" ..X_Eraser, (129*3)+64, 64, 0, 4, 4)
-  love.graphics.print("O Eraser left" ..O_Eraser, (129*3)+64, 96, 0, 4, 4)
+  if InGame then
+  
+    local testString = "Player " ..Turn
+    love.graphics.print(testString, (129*3)+64, 32, 0, 4, 4)
+    love.graphics.print("X Eraser left" ..X_Eraser, (129*3)+64, 64, 0, 4, 4)
+    love.graphics.print("O Eraser left" ..O_Eraser, (129*3)+64, 96, 0, 4, 4)
 
-  for i = 0, 2, 1 do
-    love.graphics.line((129+(129*i)), 0, (129+(129*i)), (129*3))
-    love.graphics.line(0, (129+(129*i)), (129*3), (129+(129*i)))
-  end
-  for i = 0, 2, 1 do
-    for j = 0, 2, 1 do
-      if Grid[i][j] == "X" then
-        love.graphics.draw(X_sprite, (i*128), (j*128))
-      end
-      if Grid[i][j] == "O" then
-        love.graphics.draw(O_sprite, (i*128), (j*128))
+    for i = 0, 2, 1 do
+      love.graphics.line((129+(129*i)), 0, (129+(129*i)), (129*3))
+      love.graphics.line(0, (129+(129*i)), (129*3), (129+(129*i)))
+    end
+    for i = 0, 2, 1 do
+      for j = 0, 2, 1 do
+        if Grid[i][j] == "X" then
+          love.graphics.draw(X_sprite, (i*128), (j*128))
+        end
+        if Grid[i][j] == "O" then
+          love.graphics.draw(O_sprite, (i*128), (j*128))
+        end
       end
     end
   end
   if Win then
-    love.graphics.print("WIIIN of someone... maybe? I think so?", 129*3, 129*2, 0, 8, 8)
-  end
+    love.graphics.clear()
+    love.graphics.print("WIIIN of someone... maybe? I think so?", 64, 129*2, 0, 3, 3)
+   end
 end
 
 function love.keypressed(key)
@@ -53,37 +63,39 @@ function love.keypressed(key)
 end
 
 function love.mousepressed(x, y, button, istouch)
-  if x < (129*3) and y < (129*3) then
-    local tx = math.floor(x / 128)
-    local ty = math.floor(y / 128)
+  if InGame then
+    if x < (129*3) and y < (129*3) then
+      local tx = math.floor(x / 128)
+      local ty = math.floor(y / 128)
 
-    if button == 1 then
-      if Grid[tx][ty] == "NIL" then
-        Grid[tx][ty] = Turn
-        if (Grid[0][0] == Turn and Grid[1][1] == Turn and Grid[2][2] ==) Turn) or (Grid[0][0] == Turn and Grid[0][1] == Turn and Grid[0][2] == Turn) or (Grid[1][0] == Turn and Grid[1][1] == Turn and Grid[1][2] == Turn) or (Grid[2][0] == Turn and Grid[2][1] == Turn and Grid[2][2] == Turn) or (Grid[0][0] == Turn and Grid[1][0] == Turn and Grid[2][0] == Turn) or (Grid[0][1] == Turn and Grid[1][1] == Turn and Grid[2][1] == Turn) or (Grid[0][2] == Turn and Grid[1][2] == Turn and Grid[2][2] == Turn) then
-          Win = true
-          return
+      if button == 1 then
+        if Grid[tx][ty] == "NIL" then
+          Grid[tx][ty] = Turn
+          if (Grid[0][0] == Turn and Grid[1][1] == Turn and Grid[2][2] == Turn) or (Grid[0][0] == Turn and Grid[0][1] == Turn and Grid[0][2] == Turn) or (Grid[1][0] == Turn and Grid[1][1] == Turn and Grid[1][2] == Turn) or (Grid[2][0] == Turn and Grid[2][1] == Turn and Grid[2][2] == Turn) or (Grid[0][0] == Turn and Grid[1][0] == Turn and Grid[2][0] == Turn) or (Grid[0][1] == Turn and Grid[1][1] == Turn and Grid[2][1] == Turn) or (Grid[0][2] == Turn and Grid[1][2] == Turn and Grid[2][2] == Turn) then
+            Win = true
+            return
+          end
+          if Turn == "X" then
+            Turn = "O"
+          else
+            Turn = "X"
+          end
         end
+      end
+      if button == 2 then
         if Turn == "X" then
-          Turn = "O"
+          if X_Eraser <= 0 then
+            return
+          end
+          X_Eraser = X_Eraser - 1
         else
-          Turn = "X"
+          if O_Eraser <= 0 then
+            return
+          end
+          O_Eraser = O_Eraser - 1
         end
+        Grid[tx][ty] = "NIL"
       end
-    end
-    if button == 2 then
-      if Turn == "X" then
-        if X_Eraser <= 0 then
-          return
-        end
-        X_Eraser = X_Eraser - 1
-      else
-        if O_Eraser <= 0 then
-          return
-        end
-        O_Eraser = O_Eraser - 1
-      end
-      Grid[tx][ty] = "NIL"
     end
   end
 end

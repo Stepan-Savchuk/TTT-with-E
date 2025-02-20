b2ad441bef62287b6a906558eccac7fa503ee1fc
Stepan-Savchuk <stepan_savchuk3@protonmail.com>
2025-02-20 20:55:44 +0200

    reworking check4win

--- main.lua
+++ main.lua
@@ -6,11 +6,14 @@ function love.load()
   X_sprite = love.graphics.newImage("X2.png")
   O_sprite = love.graphics.newImage("O2.png")
 
+  mx = 3
+  my = 3
+
   Grid = {}
 
-  for i = 0, 2, 1 do
+  for i = 0, mx-1, 1 do
     Grid[i] = {}
-    for j = 0, 2, 1 do
+    for j = 0, mx-1, 1 do
       Grid[i][j] = "NIL"
     end
   end
@@ -54,8 +57,8 @@ function love.draw()
       love.graphics.line((129+(129*i)), 0, (129+(129*i)), (129*3))
       love.graphics.line(0, (129+(129*i)), (129*3), (129+(129*i)))
     end
-    for i = 0, 2, 1 do
-      for j = 0, 2, 1 do
+    for i = 0, mx-1, 1 do
+      for j = 0, my-1, 1 do
         if Grid[i][j] == "X" then
           love.graphics.draw(X_sprite, (i*128), (j*128))
         end
@@ -84,7 +87,7 @@ end
 
 function love.mousepressed(x, y, button, istouch)
   if State == 1 then
-    if x < (129*3) and y < (129*3) then
+    if x < (129*mx) and y < (129*my) then
       local tx = math.floor(x / 128)
       local ty = math.floor(y / 128)
 
@@ -121,8 +124,8 @@ function love.mousepressed(x, y, button, istouch)
 end
 
 function ai()
-  for i = 0, 2, 1 do
-    for j = 0, 2, 1 do
+  for i = 0, mx-1, 1 do
+    for j = 0, my-1, 1 do
       if Grid[i][j] == "NIL" then
         Grid[i][j] = "O"
         return
@@ -131,8 +134,18 @@ function ai()
   end
 end
 
-function check4win()
-  if ((Grid[0][0] == "X" and Grid[1][1] == "X" and Grid[2][2] == "X") or (Grid[0][0] == "X" and Grid[0][1] == "X" and Grid[0][2] == "X") or (Grid[1][0] == "X" and Grid[1][1] == "X" and Grid[1][2] == "X") or (Grid[2][0] == "X" and Grid[2][1] == "X" and Grid[2][2] == "X") or (Grid[0][0] == "X" and Grid[1][0] == "X" and Grid[2][0] == "X") or (Grid[0][1] == "X" and Grid[1][1] == "X" and Grid[2][1] == "X") or (Grid[0][2] == "X" and Grid[1][2] == "X" and Grid[2][2] == "X")) or ((Grid[0][0] == "O" and Grid[1][1] == "O" and Grid[2][2] == "O") or (Grid[0][0] == "O" and Grid[0][1] == "O" and Grid[0][2] == "O") or (Grid[1][0] == "O" and Grid[1][1] == "O" and Grid[1][2] == "O") or (Grid[2][0] == "O" and Grid[2][1] == "O" and Grid[2][2] == "O") or (Grid[0][0] == "O" and Grid[1][0] == "O" and Grid[2][0] == "O") or (Grid[0][1] == "O" and Grid[1][1] == "O" and Grid[2][1] == "O") or (Grid[0][2] == "O" and Grid[1][2] == "O" and Grid[2][2] == "O")) then
-    return true
+function check4win(cx, cy, maxx, maxy)
+--  if ((Grid[0][0] == "X" and Grid[1][1] == "X" and Grid[2][2] == "X") or (Grid[0][0] == "X" and Grid[0][1] == "X" and Grid[0][2] == "X") or (Grid[1][0] == "X" and Grid[1][1] == "X" and Grid[1][2] == "X") or (Grid[2][0] == "X" and Grid[2][1] == "X" and Grid[2][2] == "X") or (Grid[0][0] == "X" and Grid[1][0] == "X" and Grid[2][0] == "X") or (Grid[0][1] == "X" and Grid[1][1] == "X" and Grid[2][1] == "X") or (Grid[0][2] == "X" and Grid[1][2] == "X" and Grid[2][2] == "X")) or ((Grid[0][0] == "O" and Grid[1][1] == "O" and Grid[2][2] == "O") or (Grid[0][0] == "O" and Grid[0][1] == "O" and Grid[0][2] == "O") or (Grid[1][0] == "O" and Grid[1][1] == "O" and Grid[1][2] == "O") or (Grid[2][0] == "O" and Grid[2][1] == "O" and Grid[2][2] == "O") or (Grid[0][0] == "O" and Grid[1][0] == "O" and Grid[2][0] == "O") or (Grid[0][1] == "O" and Grid[1][1] == "O" and Grid[2][1] == "O") or (Grid[0][2] == "O" and Grid[1][2] == "O" and Grid[2][2] == "O")) then
+ --   
+ --   return true
+--  end
+  --
+  --new algorithm for checking win is to through the row and column on which last clicked element was clicked on
+  --
+  --
+  for i = 0, maxx, 1 do
+    for j = 0, maxy, 1 do
+      
+    end
   end
 end
